43d9a62e8280b3e2219f159f8d9d3e062d2e16c7
costa92 <944310536@qq.com>
2024-12-08 21:15:53 +0800

    refactor: add transformeis

--- rag/query_encoder.py
+++ rag/query_encoder.py
@@ -2,11 +2,12 @@
 from transformers import BertModel, BertTokenizer
 
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+# 加载预训练的BERT模型和分词器，并设置clean_up_tokenization_spaces=False，以避免在分词过程中自动删除多余的空格。
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased",clean_up_tokenization_spaces=False)
 model = BertModel.from_pretrained("bert-base-uncased")
 
 
 query = "What is the capital of France?"
-inputs = tokenizer(query, return_tensors="pt", clean_up_tokenization_spaces=True)
+inputs = tokenizer(query, return_tensors="pt")
 outputs = model(**inputs).last_hidden_state.mean(dim=1)
 print(outputs)
