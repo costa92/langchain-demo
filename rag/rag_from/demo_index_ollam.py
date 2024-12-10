2b3b48420f5c0e6be1d52ca77037859f8380c533
costa <costa@helltalk.com>
2024-12-10 12:10:51 +0800

    feat: add agent

--- rag/rag_from/demo_index_ollam.py
+++ rag/rag_from/demo_index_ollam.py
@@ -1,11 +1,10 @@
 
 import os
 from langchain_openai import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 from langchain.schema import HumanMessage
 from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableSequence
 
 from dotenv import find_dotenv, load_dotenv
 
@@ -18,21 +17,30 @@ LANGCHAIN_API_KEY=os.environ['LANGCHAIN_API_KEY']
 # chat = ChatOpenAI(model="llama3.2-vision:11b", api_key="ollama", base_url="http://localhost:11434/v1/")
 chat = ChatOpenAI(model="llama3:8b", api_key="ollama", base_url="http://localhost:11434/v1/")
 
-
-
+# ===================================================== 串联提示词 ==============================
 template='''
-        你的名字是喵喵,当回答问题的时候,你都会在开头加上'喵喵~',然后再回答{question}
+        你的名字是喵喵,当回答问题的时候使用中文,你都会在开头加上'喵喵~',然后再回答{question}
     '''
+
+#这个question就是用户输入的内容,这行代码不可缺少
 prompt=PromptTemplate(
         template=template,
-        input_variables=["question"]#这个question就是用户输入的内容,这行代码不可缺少
+        input_variables=["question"]
 )
-chain = LLMChain(#将llm与prompt联系起来
-        llm=chat,
-        prompt=prompt
-        )
+
+# #使用链将它们串联起来
+# chain = LLMChain(
+#         llm=chat,
+#         prompt=prompt
+#         )
+
+# 使用 RunnableSequence 替代 LLMChain  
+chain = prompt | chat
+
 question='你是谁'
 
-res=chain.invoke(question)#运行
+res=chain.invoke({"question": question})#运行
     
-print(res['text'])#打印结果
+print(res.content)#打印结果 
+
+# ===================================================== 串联提示词 ============================== 
