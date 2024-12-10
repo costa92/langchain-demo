55727ad05785f8c5a5c7c13d57922e98da00d2d0
costa <costa@helltalk.com>
2024-12-10 10:49:37 +0800

    feat: add agent

--- rag/rag_from/demo_index.py
+++ rag/rag_from/demo_index.py
@@ -8,8 +8,7 @@ load_dotenv(find_dotenv())
 api_key =  os.getenv("OPENAI_API_KEY")
 base_url="https://api.siliconflow.cn/v1/"
 
-#设置langSmith的环境变量,是统计
-LANGCHAIN_TRACING_V2=os.environ['LANGCHAIN_TRACING_V2']
+#设置langSmith的环境变量,是统计 统计连接：https://smith.langchain.com/o/65f15b8c-1d03-43e6-b252-b6237268ba65/projects/p/5f13f7e4-cbc1-4c48-bf01-923f27aefccf?timeModel=%7B%22duration%22%3A%227d%22%7D&peek=47b3d8ac-efb4-4a9b-be56-5610db4a82b7
 LANGCHAIN_ENDPOINT=os.environ['LANGCHAIN_ENDPOINT']
 LANGCHAIN_API_KEY=os.environ['LANGCHAIN_API_KEY']
 
