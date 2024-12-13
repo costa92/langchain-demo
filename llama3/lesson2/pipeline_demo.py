65432f311f3ec6e8ff37a640411806a068a45491
costa <costa@helltalk.com>
2024-12-13 13:02:24 +0800

    fix: add pipeline

--- llama3/lesson2/pipeline_demo.py
+++ llama3/lesson2/pipeline_demo.py
@@ -2,9 +2,9 @@ import torch
 
 from transformers import pipeline
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-model_name = "Qwen/Qwen2.5-7B-Instruct"
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
+model_name = "Qwen/Qwen2-VL-2B-Instruct"
 
 messages = [ {"role": "user", "content": "请写一首赞美秋天的五言绝句"},]
 
---
+++ llama3/lesson2/pipeline_two.py
@@ -0,0 +1,17 @@
+
+
+from transformers import pipeline
+
+
+
+def data():
+  for i in range(10):
+    yield f"My example {i}"
+
+
+pipe = pipeline(model="openai-community/gpt2", device=0)
+generated_characters = 0
+for out in pipe(data()):
+    generated_characters += len(out[0]["generated_text"])
+
+print(generated_characters)
