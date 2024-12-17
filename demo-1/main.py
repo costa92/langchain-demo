c5aa91e33e364bf9e7d783c0ac1e64204656c2d9
costa92 <944310536@qq.com>
2024-12-17 22:07:30 +0800

    refactor: add pipeline

--- demo-1/main.py
+++ demo-1/main.py
@@ -1,5 +1,8 @@
+from turtle import mode
 from langchain_ollama import OllamaLLM
 
-
-llm = OllamaLLM(model="llama3:8b", temperature=0)
+model_name = "llama3"
+temperature = 0
+llm = OllamaLLM(model=model_name, temperature=temperature)
 print(llm.invoke("tell me a joke about bear."))
+  
